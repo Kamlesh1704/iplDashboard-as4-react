82020c12b74180ee6956aee9aaf705679da8c87e
Kamlesh Chandel <119688109+Kamlesh1704@users.noreply.github.com>
2024-02-27 00:27:24 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/team-matches/:id" component={TeamMatches} />
       <Route component={NotFound} />
     </Switch>
-  </BrowserRouter>
 )
 
 export default App
